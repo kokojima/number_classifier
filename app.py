94061258fe4021749079079cc7cbf274af491c8b
kokojima <92243278+kokojima@users.noreply.github.com>
2022-11-15 16:08:29 +0900

    Add files via upload
    changed to canvas interface

--- app.py
+++ app.py
@@ -7,56 +7,70 @@ import pandas as pd
 from pytube import YouTube
 import base64
 from io import BytesIO
+from streamlit_drawable_canvas import st_canvas
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.sidebar.title("画像認識アプリ")
-st.sidebar.write("オリジナルの画像認識モデルを使って何の数字の画像かを判定します。")
+st.title("手書き数字認識アプリ")
+st.write("白枠の中に0から9までの数字を一つ描いて、「Go!」をクリック！")
 
-st.sidebar.write("")
+# Specify canvas parameters in application
+drawing_mode = "freedraw"
+stroke_width = 18
+stroke_color = "#eeeeee"
+bg_color = "#000000"
+realtime_update = True
 
-img_source = st.sidebar.radio("画像のソースを選択してください。",
-                              ("画像をアップロード", "カメラで撮影"))
-if img_source == "画像をアップロード":
-    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg", "jpeg"])
-elif img_source == "カメラで撮影":
-    img_file = st.camera_input("カメラで撮影")
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    update_streamlit=realtime_update,
+    height=150,
+    width=150,
+    drawing_mode=drawing_mode,
+    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    key="canvas",
+)
 
-if img_file is not None:
-    with st.spinner("推定中..."):
-        img = Image.open(img_file)
-        st.image(img, caption="対象の画像", width=480)
-        st.write("")
+# Do something interesting with the image data and paths
+img_file = canvas_result.image_data
+if st.button('Go!'):
+      with st.spinner("推定中..."):
+          #img = Image.open(img_file)
+          img =  Image.fromarray(img_file)
+ #         st.image(img, caption="対象の画像", width=480)
+          st.write("")
 
-        # 予測
-        results = predict(img)
+          # 予測
+          results = predict(img)
 
-        # 結果の表示
-        st.subheader("判定結果")
-        start_time=[4,0,2,4,6,10,13,4,19,23]
-        url=[
-          "https://youtu.be/cPSnkQVhJHw?t=4",
-          "https://youtu.be/g89DfaOLtYA",
-          "https://youtu.be/g89DfaOLtYA?t=2",
-          "https://youtu.be/g89DfaOLtYA?t=4",
-          "https://youtu.be/g89DfaOLtYA?t=6",
-          "https://youtu.be/g89DfaOLtYA?t=10",
-          "https://youtu.be/g89DfaOLtYA?t=13",
-          "https://youtu.be/Up8IwPj5VYY?t=4",
-          "https://youtu.be/g89DfaOLtYA?t=19",
-          "https://youtu.be/g89DfaOLtYA?t=23"]
-        n_top = 3  # 確率が高い順に3位まで返す
-        for result in results[:n_top]:
-            st.write(str(round(result[1]*100, 2)) + "%の確率で" + result[0] + "です。")
-            path = url[int(result[0])]
-            st.video(path,'video/mp4',start_time[int(result[0])])             
+          # 結果の表示
+          st.subheader("判定結果")
+          start_time=[4,0,2,4,6,10,13,4,19,23]
+          url=[
+            "https://youtu.be/cPSnkQVhJHw?t=4",
+            "https://youtu.be/g89DfaOLtYA",
+            "https://youtu.be/g89DfaOLtYA?t=2",
+            "https://youtu.be/g89DfaOLtYA?t=4",
+            "https://youtu.be/g89DfaOLtYA?t=6",
+            "https://youtu.be/g89DfaOLtYA?t=10",
+            "https://youtu.be/g89DfaOLtYA?t=13",
+            "https://youtu.be/Up8IwPj5VYY?t=4",
+            "https://youtu.be/g89DfaOLtYA?t=19",
+            "https://youtu.be/g89DfaOLtYA?t=23"]
+          n_top = 3  # 確率が高い順に3位まで返す
+          for result in results[:n_top]:
+              st.write(str(round(result[1]*100, 2)) + "%の確率で" + result[0] + "です。")
+              path = url[int(result[0])]
+              st.video(path,'video/mp4',start_time[int(result[0])])             
 
-st.sidebar.write("")
-st.sidebar.write("")
-
-st.sidebar.caption("""
+st.caption("""
 このアプリは、「MNIST」を訓練データとして使っています。\n
 Copyright (c) 2017 Zalando SE\n
 Released under the MIT license\n
 https://github.com/zalandoresearch/fashion-mnist#license
 """)
+
